df7b8071bf351cfa75f657ca2210264ad46f3c54
gautam-chudasama <gautam.chudasama@outlook.com>
2025-12-27 23:17:47 +0530

    fix: update AI model to gemini-3-flash-preview and refine response configuration

--- backend/src/service/service.ai.js
+++ backend/src/service/service.ai.js
@@ -4,8 +4,12 @@ const ai = new GoogleGenAI({});
 
 async function generateResponse(chatHistory) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash-lite",
+    model: "gemini-3-flash-preview",
     contents: chatHistory,
+    config: {
+      systemInstruction:
+        "Just give responses in text format only and in short sentences.",
+    },
   });
   return response.text;
 }
